313c132478395977632c62669432784cb64dd461
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-13 09:55:47 +0200

    update settings

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+const throttled = throttle(timeup, 1000);
+
+player.on('timeupdate', throttled);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function timeup(event) {
+  console.log(event);
+}
