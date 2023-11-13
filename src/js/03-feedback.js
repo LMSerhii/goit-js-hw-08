46dbbdae794e7dbe9a91e5e26878c2300c7f2464
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-13 22:08:49 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+import storageManage from './storage';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+formProlongation();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  storageManage.save(STORAGE_KEY, formData);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  storageManage.remove(STORAGE_KEY);
+  event.target.reset();
+}
+
+function formProlongation() {
+  const storagelValue = storageManage.load(STORAGE_KEY);
+
+  if (storagelValue) {
+    form.elements.email.value = storagelValue['email'];
+    form.elements.message.value = storagelValue['message'];
+  }
+}
